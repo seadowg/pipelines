8e49a3bfbd6ce0bbcd75200bfa76d6d8bce46319
Callum Stott <cstott@pivotal.io>
2016-02-10 14:12:37 +0000

    Add spark to app

--- pipelines/jvm-test-deploy-pws/app/src/main/java/com/example/App.java
+++ pipelines/jvm-test-deploy-pws/app/src/main/java/com/example/App.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import static spark.Spark.*;
+
 public class App {
   public static void main(String[] args) {
-    
+    port(Integer.parseInt(System.getenv("PORT")));
+
+    get("/", (req, res) -> "Hello, World!");
   }
 }
