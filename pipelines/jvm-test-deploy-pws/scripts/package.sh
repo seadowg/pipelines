cbe0b2f3ce273f897f3c5fe4621316f530ca0fdb
Callum Stott <cstott@pivotal.io>
2016-02-10 16:31:39 +0000

    Use new outputs feature

--- pipelines/jvm-test-deploy-pws/scripts/package.sh
+++ pipelines/jvm-test-deploy-pws/scripts/package.sh
@@ -3,5 +3,9 @@
 set -e
 export TERM=dumb # needed for Gradle: https://issues.gradle.org/browse/GRADLE-2634
 
-cd pipelines/pipelines/jvm-test-deploy-pws/app
+pushd pipelines/pipelines/jvm-test-deploy-pws/app
 ./gradlew distZip
+popd
+
+cp pipelines/pipelines/jvm-test-deploy-pws/app/manifest.yml package/manifest.yml
+cp pipelines/pipelines/jvm-test-deploy-pws/app/build/distributions/app.zip package/app.zip
