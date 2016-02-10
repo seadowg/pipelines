fca3939bb0696d1ed07a8b4aeb2e9644741ce491
Callum Stott <cstott@pivotal.io>
2016-02-10 17:22:22 +0000

    And again...

--- pipelines/jvm-test-deploy-pws/scripts/package.sh
+++ pipelines/jvm-test-deploy-pws/scripts/package.sh
@@ -3,9 +3,9 @@
 set -e
 export TERM=dumb # needed for Gradle: https://issues.gradle.org/browse/GRADLE-2634
 
-cd pipelines/pipelines/jvm-test-deploy-pws/app/
+cd github/pipelines/jvm-test-deploy-pws/app/
 ./gradlew distZip
 cd ../../../../
 
-cp pipelines/pipelines/jvm-test-deploy-pws/app/manifest.yml package/manifest.yml
-cp pipelines/pipelines/jvm-test-deploy-pws/app/build/distributions/app.zip package/app.zip
+cp github/pipelines/jvm-test-deploy-pws/app/manifest.yml package/manifest.yml
+cp github/pipelines/jvm-test-deploy-pws/app/build/distributions/app.zip package/app.zip
--- pipelines/jvm-test-deploy-pws/scripts/test.sh
+++ pipelines/jvm-test-deploy-pws/scripts/test.sh
@@ -3,5 +3,5 @@
 set -e
 export TERM=dumb # needed for Gradle: https://issues.gradle.org/browse/GRADLE-2634
 
-cd pipelines/pipelines/jvm-test-deploy-pws/app
+cd github/pipelines/jvm-test-deploy-pws/app
 ./gradlew test
