08c7a2a68eaacbe1e535eae9db9539ad48574aea
Callum Stott <cstott@pivotal.io>
2016-02-11 17:41:07 +0000

    Use bash in scripts

--- pipelines/jvm-test-deploy-pws/scripts/package.sh
+++ pipelines/jvm-test-deploy-pws/scripts/package.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 export TERM=dumb # needed for Gradle: https://issues.gradle.org/browse/GRADLE-2634
 
-cd github/apps/spark-hello-world/
+pushd github/apps/spark-hello-world/
 ./gradlew distZip
-cd ../../../../
+popd
 
 cp github/apps/spark-hello-world/manifest.yml package/manifest.yml
 cp github/apps/spark-hello-world/build/distributions/app.zip package/app.zip
--- pipelines/jvm-test-deploy-pws/scripts/test.sh
+++ pipelines/jvm-test-deploy-pws/scripts/test.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 export TERM=dumb # needed for Gradle: https://issues.gradle.org/browse/GRADLE-2634
 
-cd github/apps/spark-hello-world
+pushd github/apps/spark-hello-world
 ./gradlew test
+popd
