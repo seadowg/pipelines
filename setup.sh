94adcfaa4524eb7993c0f5e2ea28f2849a3f0c0b
Callum Stott <cstott@pivotal.io>
2016-03-08 10:16:44 +0000

    Update to 0.74.0

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ set -e
 
 vagrant up
 
-until ./fly sync
+until ./fly -t pipelines login -c http://192.168.100.4:8080
 do
   echo "Wating for VM to boot..."
 done
 
-echo y | ./fly set-pipeline -p jvm-test-deploy-pws -c pipelines/jvm-test-deploy-pws/pipeline.yml
+echo y | ./fly -t pipelines set-pipeline -p jvm-test-deploy-pws -c pipelines/jvm-test-deploy-pws/pipeline.yml
 
 echo ""
 echo ""
